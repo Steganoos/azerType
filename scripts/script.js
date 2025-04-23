d07fcf64a0238eb6212668891ddbff62f1b0381c
SCR <steganoos@gmail.com>
2025-04-23 14:26:23 +0200

    Nombreux ajouts

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,3 @@
-
 /*********************************************************************************
  * 
  * Ce fichier contient toutes les fonctions nécessaires au fonctionnement du jeu. 
@@ -10,51 +9,84 @@
  * @param {number} score : le score de l'utilisateur
  * @param {number} nbMotsProposes : le nombre de mots proposés à l'utilisateur
  */
-function aficherResultat(score, nbMotsProposes) {
+function afficherResultat(score, nbMotsProposes) {
     // Récupération de la zone dans laquelle on va écrire le score
-    let spanScore = document.querySelector(".zoneScore span");
-    // Ecriture du texte a incérer
-    let affichageScore = `${score} sur ${nbMotsProposes}`; // L’interpolation permet de générer facilement des chaînes de caractères complexes en utilisant des backticks.
+    let spanScore = document.querySelector(".zoneScore span")
+    // Ecriture du texte
+    let affichageScore = `${score} / ${nbMotsProposes}` 
     // On place le texte à l'intérieur du span. 
-    spanScore.innerText = affichageScore;
-
-    // console.log("Votre score est de " + score + " sur " + nbMotsProposes);
-    
+    spanScore.innerText = affichageScore
 }
 
+/**
+ * Cette fonction affiche une proposition, que le joueur devra recopier, 
+ * dans la zone "zoneProposition"
+ * @param {string} proposition : la proposition à afficher
+ */
 function afficherProposition(proposition) {
     let zoneProposition = document.querySelector(".zoneProposition")
     zoneProposition.innerText = proposition
 }
 
 /**
+ * Cette fonction construit et affiche l'email. 
+ * @param {string} nom : le nom du joueur
+ * @param {string} email : l'email de la personne avec qui il veut partager son score
+ * @param {string} score : le score. 
+ */
+function afficherEmail(nom, email, score) {
+    let mailto = `mailto:${email}?subject=Partage du score Azertype&body=Salut, je suis ${nom} et je viens de réaliser le score ${score} sur le site d'Azertype !`
+    location.href = mailto
+}
+
+/**
  * Cette fonction lance le jeu. 
  * Elle demande à l'utilisateur de choisir entre "mots" et "phrases" et lance la boucle de jeu correspondante
  */
-function lancerJeu(){
-    let i = 0;
-    let score = 0;
-    afficherProposition(listeMots[i]);
-    let btnValiderMot = document.getElementById("btnValiderMot");
-    let inputEcriture = document.getElementById("inputEcriture");
+function lancerJeu() {
+    // Initialisations
+    initAddEventListenerPopup()
+    let score = 0
+    let i = 0
+    let listeProposition = listeMots
+
+    let btnValiderMot = document.getElementById("btnValiderMot")
+    let inputEcriture = document.getElementById("inputEcriture")
+
+    afficherProposition(listeProposition[i])
+
+    // Gestion de l'événement click sur le bouton "valider"
     btnValiderMot.addEventListener("click", () => {
-        console.log(inputEcriture.value);
-        if (inputEcriture.value === listeMots[i]){
+        if (inputEcriture.value === listeProposition[i]) {
             score++
         }
-        i++;
-        aficherResultat(score, i)
-        inputEcriture.value = "";
-        if ( listeMots[i] === undefined){
-            afficherProposition("Le jeux est fini");
+        i++
+        afficherResultat(score, i)
+        inputEcriture.value = ''
+        if (listeProposition[i] === undefined) {
+            afficherProposition("Le jeu est fini")
             btnValiderMot.disabled = true
-
         } else {
-            afficherProposition(listeMots[i])
+            afficherProposition(listeProposition[i])
         }
-
     })
 
-    aficherResultat(score, i)
+    // Gestion de l'événement change sur les boutons radios. 
+    let listeBtnRadio = document.querySelectorAll(".optionSource input")
+    for (let index = 0; index < listeBtnRadio.length; index++) {
+        listeBtnRadio[index].addEventListener("change", (event) => {
+            // Si c'est le premier élément qui a été modifié, alors nous voulons
+            // jouer avec la listeMots. 
+            if (event.target.value === "1") {
+                listeProposition = listeMots
+            } else {
+                // Sinon nous voulons jouer avec la liste des phrases
+                listeProposition = listePhrases
+            }
+            // Et on modifie l'affichage en direct. 
+            afficherProposition(listeProposition[i])
+        })
+    }
 
+    afficherResultat(score, i)
 }
